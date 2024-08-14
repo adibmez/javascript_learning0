4d3c04e8755be31edff8c6e04c5cc3cf813f21e9
Adib <114800092+adibmez@users.noreply.github.com>
2024-08-14 21:40:45 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+// const person = {
+//   name: "John",
+//   age: 44,
+//   Address: "USA",
+// };
+
+// ------- Error. need to solve -------
+
+// document.getElementById("demo").innerHTML =
+//   "Your Name: " + person.name + "Age: " + age + "Address: " + Address;
+
+// -------------- Well Worked ----------------
+
+// let txt = "";
+
+// for (let x in person) {
+//   txt += person[x];
+// }
+
+// document.getElementById("demo1").innerHTML = txt;
+
+// let arrayOfPerson = Object.values(person);
+
+// for (nnn of arrayOfPerson) {
+//   console.log(nnn);
+//   document.getElementById("demo2").innerHTML = nnn;
+// }
+
+// const person = {
+//   name: "John",
+//   age: 44,
+//   Address: "USA",
+// };
+
+// let myString = JSON.stringify(person);
+
+// document.getElementById("demo1").innerHTML = myString;
